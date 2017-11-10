9a3c4f3c120123b134e87a657fef24c528976352
Mala Nimalasuriya <m428@users.noreply.github.com>
2017-11-09 16:40:36 -0800

    refactor solution 16

--- euler016.js
+++ euler016.js
@@ -18,3 +18,21 @@ function findSum(num, pow) {
 };
 
 findSum(2, 5);
+
+
+// Refactored 
+function findSum(num, pow) {
+
+  var sum = 0;
+  var digits = Math.pow(num, pow).toString().split(''); // get an array individual digits digits = ['3', '2']
+
+  digits.forEach(addDigits);
+
+  function addDigits(item) {
+  item = parseInt(item); // convert each string digit into a number
+  sum = sum + item // add each number
+  };
+  return sum;
+};
+
+findSum(2, 5);
