a36f559bc901020814a93bdec8b5c824f0964568
Mala Nimalasuriya <m428@users.noreply.github.com>
2017-11-16 23:56:47 -0800

    begin solution 18

--- euler018.js
+++ euler018.js
@@ -29,26 +29,6 @@
 // with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
 
 
-// const triangle = {
-//   1: 75,
-//   2: [95, 64],
-//   3: [17, 47, 82],
-//   4: [18, 35, 87, 10],
-//   5: [20, 04, 82, 47, 65],
-//   6: [19, 01, 23, 75, 03, 34],
-//   7: [88, 02, 77, 73, 07, 63, 67],
-//   8: [99, 65, 04, 28 06 16 70 92
-//   41 41 26 56 83 40 80 70 33
-//   41 48 72 33 47 32 37 16 94 29
-//   53 71 44 65 25 43 91 52 97 51 14
-//   70 11 33 28 77 73 17 78 39 68 17 57
-//   91 71 52 38 17 14 91 43 58 50 27 29 48
-//   63 66 04 68 89 53 67 30 73 16 69 87 40 31
-//   04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
-// }
-
-// Not the correct problem. Solving for maximum total by searching each adjacent row.
-
 const triangle = {
   1: [3],
   2: [7, 4],
@@ -57,31 +37,33 @@ const triangle = {
 }
 
 let maxTotal = 0;
+let reference = 0;
+let left = 0;
+let right = 0;
+let topRow = [];
+let botRow = [];
 
 for (var row in triangle) {
     if (triangle.hasOwnProperty(row)) {
-        console.log(row + " -> " + triangle[row]);
-      values = triangle[row];
-      findMaxTotal(values);
-    } else {
-      return maxTotal;
-    }
-}
-
-function findMaxTotal(array) {
-  if (array.length == 1) {
-    maxTotal = array[0];
-  } else if {
-    findMax();
-  }
-}
-
-function findMax(array) {
-  temp = array[0];
-  for (i = 1; i < array.length; i++) {
-    if (array[i] > temp) {
-      temp = array[i];
+      topRow = triangle[row];
+      botRow = triangle[row + 1];
+      findMaxTotal(topRow, botRow);
     }
   }
-  maxTotal = maxTotal + temp;
-}
+
+  function findMaxTotal(topRow, botRow) {
+    if (topRow.length == 1) {
+      reference = topRow[0];
+      if (botRow[0] > botRow[1]) {
+        maxTotal = topRow[0] + botRow[0];
+        reference = botRow[topRow.indexOf(reference)];
+        // reference = botRow[0];
+      } else {
+        maxTotal = topRow[0] + botRow[1];
+        // reference = botRow[1];
+        reference = botRow[topRow.indexOf(reference + 1 )];
+      }
+    }
+    else if {
+    }
+  }
